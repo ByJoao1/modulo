a80e51c00552e7c846b87aba7585865cac5c3fe8
João Pedro <141186203+ByJoao1@users.noreply.github.com>
2025-04-29 11:09:37 -0300

    Add files via upload

---
+++ Aula_29_04/src/br/edu/principal/Principal.java
@@ -0,0 +1,30 @@
+package br.edu.principal;
+import java.util.Scanner;
+public class Principal {
+
+	public static void Soma( int x, int y) {
+		System.out.println("Resultado: " + (x + y));
+		}
+	public static void subtração( int x, int y) {
+		System.out.println("Resultado: " + (x - y));
+		}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Digite o primeiro numero: ");
+		int x = sc.nextInt();
+		System.out.println("Digite o segundo numero: ");
+		int y = sc.nextInt();
+	
+		Soma(x,y);
+		subtração(x,y);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
